5bba2d24c12fd457efe39694e8446222ac02f909
Yuchan Han <serltretu24@gmail.com>
2025-03-16 01:08:49 +0900

    Add EmojiTranslator and GoogleSearchEngine classes for emoji conversion and search functionality

---
+++ service/search_engine.py
@@ -0,0 +1,22 @@
+from aiohttp import ClientSession, TCPConnector
+
+from app.env_validator import settings
+
+
+class GoogleSearchEngine:
+    def __init__(self) -> None:
+        self._session = ClientSession(
+            base_url="https://www.googleapis.com", connector=TCPConnector(ssl=False)
+        )
+
+    async def search(self, keyword: str) -> list[dict]:
+        async with self._session.get(
+            "/customsearch/v1",
+            params={
+                "key": settings.GOOGLE_SEARCH_API_KEY,
+                "q": keyword,
+                "cx": settings.GOOGLE_SEARCH_ENGINE_ID,
+            },
+        ) as response:
+            data = await response.json()
+            return data["items"]
